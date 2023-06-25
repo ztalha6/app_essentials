2330423ad9944e435aba4bbda49d5b7412cc026e
Muhammad Talha Siddique <ztalha6@gmail.com>
2023-06-25 17:27:05 +0500

    token manager bug fixed

--- lib/app/data/services/token_manager.dart
+++ lib/app/data/services/token_manager.dart
@@ -2,14 +2,6 @@ import 'package:app_essentials/core/interfaces/repositories/token_respository.da
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenManager implements ITokenRepository<String> {
-  static final TokenManager _singleton = TokenManager._internal();
-
-  factory TokenManager() {
-    return _singleton;
-  }
-
-  TokenManager._internal();
-
   late final SharedPreferences sharedPreferences;
   final String jwtTokenKey = 'jwtTokenKey';
 
