ddc3ead6497c35751522aa57d075f341c7fbc3cf
Muhammad Talha Siddique <ztalha6@gmail.com>
2023-08-13 15:50:26 +0500

    Release v1.0.1
    - CustomTextFormField properties enhanced.
      - showLabelAboveField property added
      - fillColor property added

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/macbook/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/macbook/Desktop/app_essentials"
+export "FLUTTER_APPLICATION_PATH=/Users/macbook/Documents/Personal/app_essentials"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=1.0.1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
