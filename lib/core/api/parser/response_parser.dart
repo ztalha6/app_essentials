698a53ae9129ebe4ed18cbc665c00550ce8dda6d
Muhammad Talha Siddique <ztalha6@gmail.com>
2023-09-14 15:34:28 +0500

    Update response_parser.dart
    - handle response function moved to separate class
    - documentation added

--- lib/core/api/parser/response_parser.dart
+++ lib/core/api/parser/response_parser.dart
@@ -1,62 +1,39 @@
+import 'package:app_essentials/core/api/response_handler/api_response_handler.dart';
 import 'package:app_essentials/core/model/enums/response_status.dart';
-import 'package:app_essentials/core/api/interceptor/api_interceptor.dart';
 import 'package:app_essentials/core/model/base_response_model.dart';
 import 'package:dio/dio.dart';
 
+/// A generic class representing a response model from an API call.
+///
+/// This class holds the [response] data, [error] message (if any), and the [status] of the response.
 class ResponseModel<T> {
+  /// The response data of generic type [T].
   T? response;
+
+  /// A string containing the error message, if any.
   String? error;
+
+  /// The status of the response.
   ResponseStatus status;
 
+  /// Creates an instance of [ResponseModel] with the provided [error], [status], and [response].
   ResponseModel({this.error, required this.status, this.response});
 
+  /// A static method to generate a [ResponseModel] from an API response.
+  ///
+  /// It takes an [apiCall] function that returns a [Response], and a [parser] function
+  /// that converts the response body to type [T].
+  ///
+  /// Returns a [ResponseModel] representing the parsed API response.
   static Future<ResponseModel<T>> fromApiResponse<T extends BaseApiResponse>(
     Future<Response> Function() apiCall,
     T Function(Map<String, dynamic> body) parser,
   ) async {
     try {
       final Response apiResponse = await apiCall();
-      return handleResponse<T>(parser, apiResponse);
+      return ApiResponseHandler<T>(parser, apiResponse).handleResponse();
     } on DioException {
       rethrow;
-    }
-  }
-
-  static ResponseModel<T> handleResponse<T extends BaseApiResponse>(
-    T Function(Map<String, dynamic> body) parser,
-    Response apiResponse,
-  ) {
-    final T response = parser(apiResponse.data as Map<String, dynamic>);
-
-    if (apiResponse.headers.value("x-auth-error") == "InvalidCredentials") {
-      return ResponseModel<T>(
-        status: ResponseStatus.responseError,
-        response: response,
-        error: response.message,
-      );
-    }
-    if (apiResponse.statusCode == 401) {
-      throw UnauthorizedException(apiResponse.requestOptions);
-    }
-    if ((apiResponse.statusCode! < 300 && apiResponse.statusCode! >= 200) ||
-        apiResponse.statusCode == 500) {
-      if (!response.status!) {
-        return ResponseModel<T>(
-          status: ResponseStatus.responseError,
-          response: response,
-          error: response.message,
-        );
-      } else {
-        return ResponseModel<T>(
-          status: ResponseStatus.success,
-          response: response,
-        );
-      }
-    } else {
-      return ResponseModel(
-        status: ResponseStatus.nullResponse,
-        error: apiResponse.data['message'] as String,
-      );
     }
   }
 }
